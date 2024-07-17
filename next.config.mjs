b6ce3b05ec8414c487217d5384ddce989569f949
DESKTOP-SDJVT54\N1ro <wester8432@gmail.com>
2024-07-17 19:23:35 +0800

    module export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+// next.config.js
+module.exports = {
+  output: "export",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/N1ro-Test" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/N1ro-Test" : "",
+};
+
 export default nextConfig;
